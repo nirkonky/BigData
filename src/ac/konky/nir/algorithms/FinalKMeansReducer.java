078d83e821378f61bbb09c96f27faa8636ed042e
kobilemberg <kobi.lemberg@gmail.com>
2016-01-22 17:58:31 -0500

    Hadoop fixed.

---
+++ src/ac/konky/nir/algorithms/FinalKMeansReducer.java
@@ -0,0 +1,30 @@
+package ac.konky.nir.algorithms;
+
+import java.io.IOException;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+import ac.konky.nir.vector.CenterCentroidWritableComparable;
+import ac.konky.nir.vector.Vector;
+
+//for commit
+
+
+
+public class FinalKMeansReducer extends Reducer<CenterCentroidWritableComparable, Vector, Text, Text> {
+
+
+	@Override
+	protected void reduce(CenterCentroidWritableComparable key, Iterable<Vector> values,Context context) throws IOException, InterruptedException {
+		
+		
+		String stock ="";
+		for(Vector v: values)
+		{
+			stock +=v.getName().toString()+",";
+		}
+		stock=stock.substring(0,stock.length()-1);
+		context.write(new Text(key.getCentroid().getCenter().getName()), new Text(stock));
+	}
+
+	
+}
